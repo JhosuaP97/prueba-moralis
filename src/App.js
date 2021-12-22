02f0a4e829fe361169794482fa135d964f649329
jhosua.tsakana <jhosua.tsakana@gmail.com>
2021-12-22 07:51:02 -0500

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { useMoralis } from "react-moralis";
+import "./App.css";
 
 function App() {
+  const { Moralis, authenticate, isAuthenticated, user } = useMoralis();
+
+  const address = user?.attributes?.ethAddress;
+  const [NFTS, setNFTS] = useState([]);
+  const [searchValue, setSearchValue] = useState("pancake");
+
+  const getAssets = async (search) => {
+    const options = {
+      q: search,
+      limit: 100,
+      filter: "name",
+    };
+
+    const nfts = await Moralis.Web3API.token.searchNFTs(options);
+    const results = await nfts.result;
+    console.log(results);
+
+    const resultToken = results.map((nft) => fixURL(nft.token_uri));
+
+    const response = await resultToken.map(async (res) => {
+      const params = { url: res };
+      console.log({ res });
+      //This function is call from moralis admin
+      try {
+        const metadata = await Moralis.Cloud.run("fetchJSON", params);
+        return (
+          metadata.data.image ||
+          metadata.data.image_url ||
+          metadata.data.imageUrl
+        );
+        console.log(metadata);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+    const final = await Promise.all(response);
+
+    setNFTS(final);
+  };
+
+  function fixURL(url) {
+    if (url.startsWith("ipfs")) {
+      return `https://ipfs.moralis.io:2053/ipfs/${url
+        .split("ipfs://ipfs/")
+        .slice(-1)}`;
+    }
+    return `${url}?format=json`;
+  }
+
+  function checkImage(image) {
+    if (image && image.startsWith("ipfs")) {
+      return `https://ipfs.moralis.io:2053/ipfs/${image
+        .split("ipfs://ipfs/")
+        .slice(-1)}`;
+    }
+
+    return image;
+  }
+
+  const handleChange = (e) => setSearchValue(e.target.value);
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <button onClick={() => authenticate()}>Authenticate</button>
+      </div>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome {address}</h1>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchValue}
+          onChange={handleChange}
+        />
+        <button type="submit" onClick={() => getAssets(searchValue)}>
+          Get assets
+        </button>
+      </form>
+
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+          gap: "1rem",
+        }}
+      >
+        {NFTS &&
+          NFTS.map((nft, i) => (
+            <div key={i}>
+              {nft !== undefined && (
+                <>
+                  <small>{i}</small>
+                  <img
+                    style={{
+                      objectFit: "cover",
+                    }}
+                    width="100"
+                    height="100"
+                    key={i}
+                    src={checkImage(nft)}
+                    alt="nft"
+                  />
+                </>
+              )}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider
+      appId="J1TwGvW0mQaHQTcqwrBambgEwm2smJhRHhboVOXk"
+      serverUrl="https://emn4iqbi0gl3.usemoralis.com:2053/server"
+    >
+      <App />
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
